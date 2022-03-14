011dbe9dd6a58fdc0f4d0ae7d8766783a51bc301
Kyung Ho, Park <crorowslowpp@gmail.com>
2022-03-14 16:29:17 +0900

    Refactor: Removed local movies-api

--- resolver/resolvers.js
+++ resolver/resolvers.js
@@ -1,18 +1,11 @@
 import { owner } from "../database/owner.js";
 import { getFriends, getFriendById } from "../database/friends.js";
-import { getMovies, getMovieById, addMovie, deleteMovieById } from "../database/movies.js";
 
 const resolvers = {
   Query: {
     owner: () => owner,
     friends: () => getFriends(),
     friend: (_, { id }) => getFriendById(id),
-    movies: () => getMovies(),
-    movie: (_, { id }) => getMovieById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovieById(id),
   },
 };
 
